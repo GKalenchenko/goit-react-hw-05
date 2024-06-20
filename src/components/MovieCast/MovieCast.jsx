42acac7c7c410621553f94e72915c8cbb73b42fb
Hlib Kalenchenko <gkalenchenko@gmail.com>
2024-06-20 21:57:27 +0200

    adds styles for movie cast component

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { getMovieCredits } from "../../getMovies";
 import { useEffect, useState } from "react";
+import css from "../MovieCast/MovieCast.module.css";
 
 export default function MovieCast() {
   const { movieId } = useParams();
@@ -19,24 +20,26 @@ export default function MovieCast() {
   }, [movieId]);
 
   return (
-    <ul>
+    <ul className={css.list}>
       {movieCast.length > 0 ? (
         movieCast.map(({ id, name, profile_path, character }) => {
           return (
-            <li key={id}>
+            <li key={id} className={css.item}>
               <img
                 src={
-                  profile_path &&
-                  `https://image.tmdb.org/t/p/w200/${profile_path}`
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w200/${profile_path}`
+                    : "https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg"
                 }
+                className={css.image}
               />
-              <h3>{name}</h3>
-              <p>Character {character}</p>
+              <h3 className={css.name}>{name}</h3>
+              <p className={css.character}>Character {character}</p>
             </li>
           );
         })
       ) : (
-        <p>Cast information not available</p>
+        <p className={css.no_cast}>Cast information not available</p>
       )}
     </ul>
   );
