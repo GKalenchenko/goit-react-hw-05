84cd2efbde18c78b848d6f8a2ab31ee531dd344a
Hlib Kalenchenko <gkalenchenko@gmail.com>
2024-06-20 21:55:02 +0200

    adds styles for movie navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,26 @@
 import { NavLink } from "react-router-dom";
+import css from "../Navigation/Navigation.module.css";
+import clsx from "clsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function Navigation() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/movies">Movies</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav className={`container ${css.nav}`}>
+      <ul className={css.list}>
+        <li className={css.item}>
+          <NavLink className={buildLinkClass} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className={css.item}>
+          <NavLink className={buildLinkClass} to="/movies">
+            Movies
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
